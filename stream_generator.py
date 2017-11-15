c275c7b784b563819ec5fdcfc86c38f821e6f3f2
Lam Chi Thanh <E0008973@u.nus.edu>
2017-11-16 01:20:30 +0800

    Add script to generate stream

---
+++ stream_generator.py
@@ -0,0 +1,12 @@
+from random import randint
+
+def generate_stream(max_index, num_ops):
+	bits = [False] * max_index
+	with open("data_stream.txt", "w") as f:
+		for i in range(num_ops):
+			index = randint(0, max_index - 1)
+			update = -1 if bits[index] else 1
+			bits[index] = not bits[index]
+			print(str(index+1) + " " + str(update) + " ", file=f, end="")
+
+generate_stream(10, 10)
