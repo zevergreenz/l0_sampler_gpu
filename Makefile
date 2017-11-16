fb2daaf8177828d36f58cdac82ea0aa3edb82047
Yuchuan <yyc478@gmail.com>
2017-11-15 23:45:44 -0500

    Try with unsigned integers

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 cuda_build:
 	mkdir -p bin out
-	nvcc -o bin/s_sparse_sampler -arch=sm_32 -lm -ccbin clang-3.8 s_sparse_sampler.cu
+	nvcc -o bin/s_sparse_sampler -arch=sm_32 -lm -g -ccbin clang-3.8 s_sparse_sampler.cu
 
 cuda: cuda_build
 	# /usr/bin/time -f "Real Time %e\nCPU Time %U \nKernel Time %S\nContext Switches %w" bin/mm-cuda 1000;
-	#bin/mm-cuda in/10int.dat;
+	bin/s_sparse_sampler ../data_stream.txt;
--- s_sparse_sampler.cu
+++ s_sparse_sampler.cu
@@ -13,51 +13,51 @@
 
 typedef struct
 {
-  int sum;
-  int weight;
-  int fingerprint;
+  unsigned int sum;
+  unsigned int weight;
+  unsigned int fingerprint;
 } one_sparse_sampler;
 
 typedef struct
 {
   // Number of hash functions
-  int k;
+  unsigned int k;
 
   // Number of one-sparse samplers in each row
-  int s;
+  unsigned int s;
 
   // Number of coefficients in the hash function
-  int numCoefficients;
+  unsigned int numCoefficients;
 
   // coefficients
-  int                *coefficients;
+  unsigned int *coefficients;
   one_sparse_sampler *samplers;
 } s_sparse_sampler;
 
-int hash(int *coeff, int numCoefficients, int value) {
-  int val = value;
+unsigned int hash(unsigned int *coeff, unsigned int numCoefficients, unsigned int value) {
+  unsigned int val = value;
 
-  for (int i = 0; i < numCoefficients; i++) {
-    val = (val * coeff[i] + val) % P;
-  } 
-  return (val < 0) ? val + P : val;
-}
-
-__device__ void hash_gpu(int &hashVal, int *coeff, int numCoefficients, int value) {
-  int val = value;
-
-  for (int i = 0; i < numCoefficients; i++) {
+  for (unsigned int i = 0; i < numCoefficients; i++) {
     val = (val * coeff[i] + val) % P;
   }
-  hashVal = (val < 0) ? val + P : val;
+  return val;
 }
 
-void process(s_sparse_sampler sampler, int *buffer) {
-  int index, update, hashVal;
+__device__ unsigned int hash_gpu(unsigned int *coeff, unsigned int numCoefficients, unsigned int value) {
+  unsigned int val = value;
+
+  for (unsigned int i = 0; i < numCoefficients; i++) {
+    val = (val * coeff[i] + val) % P;
+  }
+  return val;
+}
+
+void process(s_sparse_sampler sampler, unsigned int *buffer) {
+  unsigned int index, update, hashVal;
   one_sparse_sampler *one_sampler;
 
-  for (int j = 0; j < BUFFER_SIZE / 2; j++) {
-    for (int i = 0; i < sampler.k; i++) {
+  for (unsigned int j = 0; j < BUFFER_SIZE / 2; j++) {
+    for (unsigned int i = 0; i < sampler.k; i++) {
       index   = buffer[j >> 1];
       update  = buffer[1 + (j >> 1)];
       hashVal = hash(&(sampler.coefficients[i * sampler.numCoefficients]),
@@ -71,29 +71,38 @@ void process(s_sparse_sampler sampler, int *buffer) {
   }
 }
 
-
-
-__global__ void process_gpu(s_sparse_sampler sampler, int *buffer) {
-  int i, index, update, hashVal;
-  one_sparse_sampler *one_sampler;
-
-  i = blockIdx.x;
-
-  for (int j = 0; j < BUFFER_SIZE / 2; j++) {
-    index  = buffer[j >> 1];
-    update = buffer[1 + (j >> 1)];
-    hash_gpu(hashVal,
-             &(sampler.coefficients[i * sampler.numCoefficients]),
-             sampler.numCoefficients,
-             index);
-    hashVal                  %= (2 * sampler.s);
-    one_sampler               = &sampler.samplers[i * 2 * sampler.s + hashVal];
-    one_sampler->weight      += update;
-    one_sampler->sum         += index * update;
-    // This is slow! pow() only exists for single and double-precision floats
-    // And we need a double to cover the range of integers
-    one_sampler->fingerprint += (update * pow((double)Z, (double)index));
+__device__ unsigned int powMod(unsigned int z, unsigned int index) {
+  unsigned int subpow;
+  if(index == 0) {
+    return 1;
+  } else if(index & 1 == 1) {
+    subpow = powMod(z, index << 1);
+    return (z * subpow * subpow) % P;
+  } else {
+    subpow = powMod(z, index << 1);
+    return (subpow * subpow) % P;
   }
+}
+
+__global__ void process_gpu(s_sparse_sampler sampler, unsigned int *buffer) {
+  // unsigned int i, index, update, hashVal;
+  // one_sparse_sampler *one_sampler;
+  //
+  // i = blockIdx.x;
+  //
+  // for (unsigned int j = 0; j < BUFFER_SIZE / 2; j++) {
+  //   index  = buffer[j >> 1];
+  //   update = buffer[1 + (j >> 1)];
+  //   hashVal = hash_gpu( &(sampler.coefficients[i * sampler.numCoefficients]),
+  //            sampler.numCoefficients,
+  //            index) % (2 * sampler.s);
+  //   one_sampler               = &sampler.samplers[i * 2 * sampler.s + hashVal];
+  //   one_sampler->weight      += update;
+  //   one_sampler->sum         += index * update;
+  //   // This is slow! pow() only exists for single and double-precision floats
+  //   // And we need a double to cover the range of unsigned integers
+  //   one_sampler->fingerprint += (update * powMod(Z, index));
+  // }
 }
 
 /**
@@ -102,22 +111,22 @@ __global__ void process_gpu(s_sparse_sampler sampler, int *buffer) {
    As of now, this method may contain duplicated indices.
    TODO: Remove duplicates.
  */
-int* query(s_sparse_sampler sampler, int& size) {
-  int *result = (int *)malloc(2 * sampler.s * sampler.k * sizeof(int));
+unsigned int* query(s_sparse_sampler sampler, unsigned int& size) {
+  unsigned int *result = (unsigned int *)malloc(2 * sampler.s * sampler.k * sizeof(unsigned int));
 
   size = 0;
   one_sparse_sampler *one_sampler;
 
-  for (int i = 0; i < sampler.k; i++) {
-    for (int j = 0; j < sampler.s * 2; j++) {
+  for (unsigned int i = 0; i < sampler.k; i++) {
+    for (unsigned int j = 0; j < sampler.s * 2; j++) {
       one_sampler = &sampler.samplers[i * 2 * sampler.s + j];
 
       if (one_sampler->weight != 0) {
-        int index = one_sampler->sum / one_sampler->weight;
-        int error = one_sampler->fingerprint -
+        unsigned int index = one_sampler->sum / one_sampler->weight;
+        unsigned int error = one_sampler->fingerprint -
                     ((one_sampler->weight * pow(Z, index)));
 
-        if ((int)error == 0) result[size++] = index;
+        if ((unsigned int)error == 0) result[size++] = index;
       }
     }
   }
@@ -125,32 +134,32 @@ int* query(s_sparse_sampler sampler, int& size) {
 }
 
 void initialize_s_sparse_sampler(s_sparse_sampler *sampler,
-                                 int               s,
-                                 int               k,
-                                 int               n) {
+                                 unsigned int               s,
+                                 unsigned int               k,
+                                 unsigned int               n) {
   sampler->k               = k;
   sampler->s               = s;
   sampler->numCoefficients = n;
-  cudaMallocManaged((void **)&(sampler->samplers),     sizeof(int) * k * s * 2);
-  cudaMallocManaged((void **)&(sampler->coefficients), sizeof(int) * k * n);
+  cudaMallocManaged((void **)&(sampler->samplers),     sizeof(unsigned int) * k * s * 2);
+  cudaMallocManaged((void **)&(sampler->coefficients), sizeof(unsigned int) * k * n);
 
-  for (int i = 0; i < k * n; i++) {
+  for (unsigned int i = 0; i < k * n; i++) {
     sampler->coefficients[i] = rand();
   }
 }
 
-void sample(char *filename, int s, int k) {
+void sample(char *filename, unsigned int s, unsigned int k) {
   s_sparse_sampler sampler;
-  int *buffer;
+  unsigned int *buffer;
 
   initialize_s_sparse_sampler(&sampler, s, k, NUMCOEFF);
-  cudaMallocManaged((void **)&buffer, sizeof(int) * BUFFER_SIZE);
+  cudaMallocManaged((void **)&buffer, sizeof(unsigned int) * BUFFER_SIZE);
 
   // Read data from file
   FILE *fdIn = fopen(filename, "r");
 
   while (!feof(fdIn)) {
-    fscanf(fdIn, "%d %d", &buffer[0], &buffer[1]);
+    fscanf(fdIn, "%u %u", &buffer[0], &buffer[1]);
 
     for (int i = 0; i < BUFFER_SIZE / 2; i++) {
       // Sequencial processing
@@ -165,10 +174,10 @@ void sample(char *filename, int s, int k) {
   cudaDeviceSynchronize();
 
   // Query the s-sparse sampler and print out
-  int  size   = 0;
-  int *result = query(sampler, size);
+  unsigned int  size   = 0;
+  unsigned int *result = query(sampler, size);
 
-  for (int i = 0; i < size; i++) printf("%d ", result[i]);
+  for (unsigned int i = 0; i < size; i++) printf("%u ", result[i]);
   printf("\n");
 
   // Clean up
